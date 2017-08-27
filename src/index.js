20246f7f7fb60b6e9360dd8e7004d73c0e0b08c6
Brahian E. Soto Mercedes <mail@soto.codes>
2017-08-27 15:38:43 +0200

    Set up react, babel and webpack.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <h1 style={{ textAlign: 'center' }}>Hello, World</h1>,
+  document.getElementById('test'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  output: {
+    filename: 'lib/bundle.js',
+    path: __dirname + '/public/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/css', to: 'css/' },
+      { from: './src/icons', to: 'icons/' },
+      { from: './src/messenger', to: 'messenger/' },
+      { from: './src/sidebar', to: 'sidebar' },
+      { from: './src/users', to: 'users' },
+    ]),
+  ],
+};
